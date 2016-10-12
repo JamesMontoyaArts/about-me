8ddbf3992eca89995a92d6ea9d04a12ba9307065
James Montoya <jamesmontoyaarts@gmail.com>
2016-10-11 20:47:23 -0600

    Created the about me html

---
+++ public_html/about_me_html.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+
+		<!--Add CSS file-->
+		<link href="" rel="" type="" />
+
+		<title>James Montoya</title>
+	</head>
+	<body>
+
+		<!--This is the header-->
+		<header>
+				<h1>Hello, My Name Is James Montoya</h1>
+		</header>
+
+		<!--This is the main page content-->
+		<main>
+
+			<!--begin sention 1-->
+			<section>
+				<p>My name is James Montoya and I am an artist.  I grew up in Santa Fe but have been living in Albuquerque NM</p>
+			</section>
+		</main>
+	</body>
+</html>
