21f05321efb7448556abf0a72ecf99342a846b0b
James Montoya <jamesmontoyaarts@gmail.com>
2016-10-13 16:38:48 -0600

    added photos and started style tag

--- public_html/index.php
+++ public_html/index.php
@@ -6,6 +6,11 @@
 		<!--This is the title-->
 		<title>James Montoya</title>
 
+		<!--This is CSS gallery-->
+		<style>
+
+		</style>
+
 	</head>
 
 	<body>
@@ -20,7 +25,30 @@
 
 			<!--begin sention 1-->
 			<section>
-				<p>My name is James Montoya and I am an artist.  I grew up in Santa Fe but have been living in Albuquerque since 2003. Growing up in Santa Fe close to the world famous Canyon Road, I was exposed to many artists and artwork from around the world.  My elementary school was very art forcused and nutured my artistic talents.  Today I show my work in Denver CO, Phoenix AZ, and of course all over Albuquerque.  I host an event once a month called Art Fight at Tractor Brewing Co.  It's a live art competition where artists compete by painting live and the beer drinking public votes for their favorite by drinking beers.  For every beer they buy they also get a ticket to vote for their favorite of the night.  At the end of three hours we count the votes and declare a winner.  We also have a live auction of the pieces that were created and donate a portion of the proceeds to an organization or cause here in town. For October's Art Fight we are helping the annual Muertos y Marigolds parade in the south valley and </p>
+				<p>
+					My name is James Montoya and I am an artist.
+					I grew up in Santa Fe but have been living in Albuquerque since 2003.
+					Growing up in Santa Fe close to the world famous Canyon Road, I was exposed to many artists and artwork from around the world.
+					My elementary school was very art focused and nutured my artistic talents.
+					I work primarily in acrylic but I have used watercolor, oil, and encaustic paints before.
+					Today I show my work in Denver CO, Phoenix AZ, and of course all over Albuquerque.
+					I used to work at Masks y Mas on Central and also as a cook at Conchita's Cafe before coming to the bootcamp.
+					I host an event once a month called Art Fight at Tractor Brewing Co.
+					It's a live art competition where artists compete by painting live and the beer drinking public votes for their favorite by drinking beers.
+					For every beer they buy they also get a ticket to vote for their favorite of the night.
+					At the end of three hours we count the votes and declare a winner.
+					We also have a live auction of the pieces that were created and donate a portion of the proceeds to an organization or cause here in town.
+					For our two year anniversary Art Fight in October, we are helping the annual Muertos y Marigolds parade in the south valley.
+					November we will be auctioning in support of the Standing Rock Sioux Tribe in North Dakota to help them through the winter.
+				</p>
+			</section>
+
+			<section>
+
+				<h2>Gallery</h2>
+
+				<img ref=""
+
 			</section>
 
 		</main>
