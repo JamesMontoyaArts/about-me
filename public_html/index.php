541f276d9bed8107ca777491fc2412e2a4db0b6e
James Montoya <jamesmontoyaarts@gmail.com>
2016-10-13 16:00:34 -0600

    committing for the lulz :joy_cat:

--- public_html/index.php
+++ public_html/index.php
@@ -20,7 +20,7 @@
 
 			<!--begin sention 1-->
 			<section>
-				<p>My name is James Montoya and I am an artist.  I grew up in Santa Fe but have been living in Albuquerque since 2003. Growing up in Santa Fe close to the world famous Canyon Road, I was exposed to many artists and artwork from around the world.  My elementary school was very art forcused and nutured my artistic talents.  Today I show my work in Denver CO, Phoenix AZ, and of course all over Albuquerque.  </p>
+				<p>My name is James Montoya and I am an artist.  I grew up in Santa Fe but have been living in Albuquerque since 2003. Growing up in Santa Fe close to the world famous Canyon Road, I was exposed to many artists and artwork from around the world.  My elementary school was very art forcused and nutured my artistic talents.  Today I show my work in Denver CO, Phoenix AZ, and of course all over Albuquerque.  I host an event once a month called Art Fight at Tractor Brewing Co.  It's a live art competition where artists compete by painting live and the beer drinking public votes for their favorite by drinking beers.  For every beer they buy they also get a ticket to vote for their favorite of the night.  At the end of three hours we count the votes and declare a winner.  We also have a live auction of the pieces that were created and donate a portion of the proceeds to an organization or cause here in town. For October's Art Fight we are helping the annual Muertos y Marigolds parade in the south valley and </p>
 			</section>
 
 		</main>
