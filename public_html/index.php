2eb2a86f237924094c65f67cde05c8cd8b6eaca8
James Montoya <jamesmontoyaarts@gmail.com>
2016-10-13 15:26:21 -0600

    deleted css from html added some notes

--- public_html/index.php
+++ public_html/index.php
@@ -3,11 +3,11 @@
 	<head>
 		<meta charset="utf-8">
 
-		<!--Add CSS file-->
-		<link href="" rel="" type="" />
-
+		<!--This is the title-->
 		<title>James Montoya</title>
+
 	</head>
+
 	<body>
 
 		<!--This is the header-->
@@ -20,8 +20,11 @@
 
 			<!--begin sention 1-->
 			<section>
-				<p>My name is James Montoya and I am an artist.  I grew up in Santa Fe but have been living in Albuquerque NM</p>
+				<p>My name is James Montoya and I am an artist.  I grew up in Santa Fe but have been living in Albuquerque since 2003. Growing up in Santa Fe close to the world famous Canyon Road, I was exposed to many artists and artwork from around the world.  My elementary school was very art forcused and nutured my artistic talents.  Today I show my work in Denver CO, Phoenix AZ, and of course all over Albuquerque.  </p>
 			</section>
+
 		</main>
+
 	</body>
+
 </html>
